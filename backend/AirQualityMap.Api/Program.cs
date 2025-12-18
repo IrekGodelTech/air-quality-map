872822f3b865a724e26b818d4155d8529c4704cd
Ireneusz Gościński <i.goscinski@godeltech.com>
2025-12-18 13:09:33 +0100

    Add authorization to swagger

--- backend/AirQualityMap.Api/Program.cs
+++ backend/AirQualityMap.Api/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using AirQualityMap.Api.Data;
 using AirQualityMap.Api.Services;
 using AirQualityMap.Api.Services.Contracts;
@@ -11,7 +12,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.\nExample: copy and paste the token value from login response",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+    
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Configure Database
 builder.Services.AddDbContext<AppDbContext>(options =>
